e25a5e71646500e92e5ff06cb2daff763b88b018
sarvin <sarvinprabu.r@skematix.in>
2025-11-24 17:09:49 +0530

    astro config file code changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  integrations: [react(), tailwind()],
   adapter: cloudflare(),
-  image: {
-    service: {
-      type: 'compile'  
-    }
-  }
+  output: 'server'
 });
