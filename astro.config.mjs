53fa7275cbbd2f40f73321e0582c7e37b743284e
sarvin <sarvinprabu.r@skematix.in>
2025-11-24 17:39:33 +0530

    cleaned up code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  adapter: cloudflare({
-    includeFiles: ['dist/assets/**/*'], // <-- include all generated CSS/JS/images
-  }),
-  output: 'server',
-  build: {
-    assets: 'assets', // make sure Astro puts CSS/JS in 'dist/assets'
-  },
+  adapter: cloudflare(),
+  output: 'server'
 });
